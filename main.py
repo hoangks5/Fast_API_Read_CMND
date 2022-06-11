c0c84f48b4a8bf9791f45fd05a223711cf444ce9
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2022-06-11 03:46:17 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, File, UploadFile #import class FastAPI() từ thư viện fastapi
 import requests
 app = FastAPI() # gọi constructor và gán vào biến app
-import json
 
 @app.get("/") # giống flask, khai báo phương thức get và url
 async def root(): # do dùng ASGI nên ở đây thêm async, nếu bên thứ 3 không hỗ trợ thì bỏ async đi
